deadabe13df81805747029fa0813e2d0ee2b3cd5
invakid404 <invakid404@riseup.net>
2024-05-22 14:26:28 +0300

    fix: normalize file name

--- scrape.go
+++ scrape.go
@@ -22,7 +22,7 @@ const (
 )
 
 var (
-	sourcePattern = regexp.MustCompile("Circle_Linux_Mac_Driver_v([\\d.]+)\\.zip$")
+	sourcePattern = regexp.MustCompile("v([\\d.]+)\\.zip$")
 )
 
 func main() {
@@ -107,11 +107,19 @@ func main() {
 			return nil
 		})
 
-		if sourcePath != "" {
+		if done && sourcePath != "" {
 			break
 		}
 
 		time.Sleep(100 * time.Millisecond)
+	}
+
+	basePath := filepath.Dir(sourcePath)
+	targetPath := filepath.Join(basePath, fmt.Sprintf("Circle_Linux_Mac_Driver_v%s.zip", version))
+
+	err = os.Rename(sourcePath, targetPath)
+	if err != nil {
+		log.Fatalln("failed to rename file:", err)
 	}
 
 	outputFilePath := os.Getenv("GITHUB_OUTPUT")
@@ -122,7 +130,7 @@ func main() {
 
 	defer outputFile.Close()
 
-	if _, err := outputFile.WriteString(fmt.Sprintf("path=%s\nversion=%s\n", sourcePath, version)); err != nil {
+	if _, err := outputFile.WriteString(fmt.Sprintf("path=%s\nversion=%s\n", targetPath, version)); err != nil {
 		log.Fatalln("failed to write output:", err)
 	}
 
